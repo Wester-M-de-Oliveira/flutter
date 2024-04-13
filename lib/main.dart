cef95e92a246b79b434b361bd3a3231cf9e4c444
Wester-M-de-Oliveira <westerjesuino@gmail.com>
2024-04-13 01:46:39 -0400

    initial view login

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,66 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MeuApp());
+  runApp(Formulario());
 }
 
-class MeuApp extends StatefulWidget {
+class Formulario extends StatefulWidget {
   @override
-  _MeuApp createState() => _MeuApp();
+  _Formulario createState() => _Formulario();
 }
 
-class _MeuApp extends State<MeuApp> {
-  var contador = 0;
-  bool test = true;
-  var truco = 'adfa';
-  var color = Colors.black;
-  var color2 = Colors.black;
+class _Formulario extends State<Formulario> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Meu App'),
-        ),
-        body: Column(children: [
-          Text(truco),
-          const Text('Olá, mundo!'),
-          Text(contador.toString(), style: const TextStyle(fontSize: 30)),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  if (test) {
-                    contador++;
-                  } else {
-                    contador--;
-                  }
-                });
-              },
-              child: const Text('Clique aqui!'),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  if (test) {
-                    test = false;
-                  } else {
-                    test = true;
-                  }
-                });
-              },
-              child: const Text('troque para alterar mais ou menos!'),
-            ),
-          ),
-          Center(
-            child: IconButton(
-              onPressed: () {
-                setState(() {
-                  contador++;
-                  color2 = Colors
-                      .primaries[Random().nextInt(Colors.primaries.length)];
-                });
-              },
-              icon: Icon(
-                Icons.add,
-                color: color,
-                size: 50,
-              ),
-            ),
-          ),
-          Center(
-            child: IconButton(
-              onPressed: () {
-                setState(() {
-                  contador--;
-                  color = Colors
-                      .primaries[Random().nextInt(Colors.primaries.length)];
-                });
-              },
-              icon: Icon(
-                Icons.remove,
-                color: color2,
-                size: 50,
-              ),
-            ),
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Digite algo',
-            ),
-            onChanged: (text) {
-              setState(() {
-                truco = text;
-              });
-            },
-          ),
-        ]),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(),
+            body: ListView(
+              padding: EdgeInsets.all(35),
+              children: [
+                Center(
+                  child: Text('Faça seu login',
+                      style: TextStyle(fontSize: 30)),
+                ),
+                SizedBox(height: 20),
+                Text('Faça login para continuar nosso aplicativo'),
+                SizedBox(height: 30),
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                      TextFormField(
+                        decoration: InputDecoration(hintText: 'informe o gmail', border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Informe o seu nome';
+                          }
+                        }),
+                        SizedBox(height: 25),
+                        TextFormField(
+                        decoration: InputDecoration(hintText: 'informe o gmail', border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)), suffix: Text('Suffix')),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Informe a senha';
+                          }
+                        })
+                        ],)),
+
+                ElevatedButton(
+                  onPressed: () {
+                    if(_formKey.currentState!.validate()) {
+
+                    }
+                  },
+                  child: const Text('troque para alterar mais ou menos!'),
+                ),
+
+              ],
+            )));
   }
 }
