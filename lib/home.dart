7390869a1acba740a957c3b33f33a3da2f9ecbfb
Wester-M-de-Oliveira <westerjesuino@gmail.com>
2024-08-16 05:36:34 -0400

    feat: Add new images for profile and countries

--- lib/home.dart
+++ lib/home.dart
@@ -12,7 +12,7 @@ class Home extends StatelessWidget {
           margin: const EdgeInsets.only(left: 16),
           padding: const EdgeInsets.fromLTRB(8,0,5,0),
           decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 239, 239, 240),
+            color: Colors.grey[200],
             borderRadius: BorderRadius.circular(1000),
           ),
           child: const Row(
@@ -31,51 +31,54 @@ class Home extends StatelessWidget {
           ),
         ),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications_outlined, color: Colors.black),
-            onPressed: () {
-              // Ação do ícone de notificação
-            },
-          ),
+          Container(
+            margin: const EdgeInsets.only(right: 16),
+            decoration: BoxDecoration(
+              color: Colors.grey[200], // Define a cor de fundo cinza
+              shape: BoxShape.circle, // Define a forma circular
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.notifications_outlined, color: Colors.black),
+              onPressed: () {
+                // Ação do ícone de notificação
+              },
+            ),
+          )
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: SingleChildScrollView(
-          child: Column(
+        // padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+        child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text(
                 'Explore o',
                 style: TextStyle(
-                  fontSize: 28,
+                  fontSize: 45,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
                 ),
               ),
-              const Row(
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
                 children: [
-                  Expanded(
-                    child: Text(
-                      'Belo mundo ',
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                  const Text('Belo mundo ', style: TextStyle(fontSize: 35)),
+                  Column(
+                    children: [
+                      const Text('world!', style: TextStyle(fontSize: 35,color: Color(0xFFFF7029))),
+                      Container(
+                        child: Image.asset(
+                          'assets/images/explorar.png',
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                  ),
-                  Text(
-                    'world!',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange,
-                    ),
-                  ),
+                    ],
+                  )
                 ],
               ),
-              const SizedBox(height: 16),
+              const SizedBox(height: 35),
               const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -98,43 +101,41 @@ class Home extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 16),
-              Container(
-                height: 250,
+              SizedBox(
+                height: 420,
                 child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    DestinationCard(
-                      assetImage: 'assets/images/espanha.png',
-                      title: 'Espanha',
-                      location: 'Madrid, Espanha',
-                      rating: 4.7,
-                    ),
-                    DestinationCard(
-                      assetImage: 'assets/images/france.png',
-                      title: 'França',
-                      location: 'Paris, França',
-                      rating: 4.8,
-                    ),
-                    // Adicione mais cards conforme necessário
-                  ],
-                ),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  DestinationCard(
+                    assetImage: 'assets/images/espanha.png',
+                    title: 'Espanha',
+                    location: 'Madrid, Espanha',
+                    rating: 4.7,
+                  ),
+                  DestinationCard(
+                    assetImage: 'assets/images/france.png',
+                    title: 'França',
+                    location: 'Paris, França',
+                    rating: 4.8,
+                  ),
+                ],
+              ),
               ),
             ],
           ),
         ),
-      ),
 
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-          const BottomNavigationBarItem(
+        items: const [
+          BottomNavigationBarItem(
             icon: Icon(Icons.message),
             label: 'Messages',
           ),
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: 'Perfil',
           ),
@@ -160,59 +161,106 @@ class DestinationCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
+      width: 300,
+      padding: const EdgeInsets.all(16),
       margin: const EdgeInsets.only(right: 16),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        image: const DecorationImage(
-          // image: AssetImage(assetImage),
-          image: AssetImage('assets/images/pageview3.png'),
-          fit: BoxFit.cover,
-        ),
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.grey[100],
       ),
-      child: Stack(
+      child: Column(
         children: [
-          const Positioned(
-            top: 16,
-            right: 16,
-            child: Icon(Icons.bookmark_border, color: Colors.white),
-          ),
-          Positioned(
-            bottom: 16,
-            left: 16,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  location,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                  ),
-                ),
-                Row(
-                  children: [
-                    const Icon(Icons.star, color: Colors.yellow, size: 16),
-                    const SizedBox(width: 4),
-                    Text(
-                      '$rating',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
                 ),
               ],
             ),
+            child: Center(
+                child: Image.asset(
+                  assetImage,
+                  fit: BoxFit.fitWidth,
+                  width: 280,
+                ),
+              )
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star, color: Color.fromARGB(255, 255, 230, 0), size: 16),
+                  const SizedBox(width: 4),
+                  Text(
+                    '$rating', // Quantidade de estrelas
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.location_on, color: Colors.black87, size: 16),
+                      const SizedBox(width: 4),
+                      Text(
+                        location,
+                        style: const TextStyle(
+                          color: Colors.black87,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 10,
+                        backgroundImage: AssetImage('assets/images/perfil.png'),
+                      ),
+                      CircleAvatar(
+                        radius: 10,
+                        backgroundImage: AssetImage('assets/images/perfil.png'),
+                      ),
+                      CircleAvatar(
+                        radius: 10,
+                        backgroundImage: AssetImage('assets/images/perfil.png'),
+                      ),
+                      Text(
+                        '+3',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
           ),
         ],
       ),
